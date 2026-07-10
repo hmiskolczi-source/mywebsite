c7c91681cfb30a8a5fb3e70b8a12d5b82d394c33
Norbert Bogdán <norbertb.consulting@gmail.com>
2026-07-10 09:10:13 +0200

    chore: auto-commit changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,9 @@ import tailwindcss from '@tailwindcss/vite';
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+  image: {
+    domains: ['bgumoxbjyuzc6ytp.public.blob.vercel-storage.com'],
+  },
   vite: {
     plugins: [tailwindcss()],
   },
