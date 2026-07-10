4d8f11af38c7548347fcfbe73eba5c09dfc41cef
Norbert Bogdán <norbertb.consulting@gmail.com>
2026-07-10 10:22:55 +0200

    chore: auto-commit changes

--- src/components/shared/ImageWatermark.tsx
+++ src/components/shared/ImageWatermark.tsx
@@ -17,10 +17,11 @@ export default function ImageWatermark({ children, className = '' }: ImageWaterm
       {children}
       <div className="absolute bottom-3 left-3 sm:bottom-4 sm:left-4 pointer-events-none">
         <img
-          src="https://bgumoxbjyuzc6ytp.public.blob.vercel-storage.com/shared/imagelogo.png"
+          {...imgProps("https://bgumoxbjyuzc6ytp.public.blob.vercel-storage.com/shared/imagelogo.png", "32px")}
           alt="Kinga Miskolczi watermark"
           className="h-6 w-auto sm:h-8 opacity-80"
           referrerPolicy="no-referrer"
+          decoding="async"
         />
       </div>
     </div>
