dd99bf03dd1d70fc63beb1c26a0c82c9bf58322e
Norbert Bogdán <norbertb.consulting@gmail.com>
2026-07-10 10:23:17 +0200

    chore: auto-commit changes

--- src/components/shared/Navbar.tsx
+++ src/components/shared/Navbar.tsx
@@ -5,6 +5,7 @@
 
 import React, { useState } from 'react';
 import { Menu, X } from 'lucide-react';
+import { imgProps } from '../../lib/imageOpt';
 
 interface NavbarProps {
   currentPage: 'home' | 'sminktetovalas' | 'sminkkepzes';
